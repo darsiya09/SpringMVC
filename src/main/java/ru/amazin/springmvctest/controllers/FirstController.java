08ee6a4de80d3bc023035f4bb2bacef1bd22d46d
Aleksey Mazin <darsiya09@gmail.com>
2022-04-10 12:02:50 +0300

    The calculator view, the calculator controller and the calculator helper were added

--- src/main/java/ru/amazin/springmvctest/controllers/FirstController.java
+++ src/main/java/ru/amazin/springmvctest/controllers/FirstController.java
@@ -1,24 +1,46 @@
 package ru.amazin.springmvctest.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.amazin.springmvctest.Helpers.Calculator;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
 
+    @Autowired
+    public void setCalculator(Calculator calculator) {
+        this.calculator = calculator;
+    }
+
+    private Calculator calculator;
+
     @GetMapping("hello")
     public String hello(@RequestParam(value = "name", required = false) String name,
-                        @RequestParam("surname") String surname) {
+                        @RequestParam(value = "surname", required = false) String surname,
+                        Model model) {
         System.out.println("Hi, " + name + " " + surname + "!");
+        model.addAttribute("message", "Hi, " + name + " " + surname + "!");
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodbye() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/simple/calculator")
+    public String simpleCalculator(@RequestParam(value = "a") int a,
+                                   @RequestParam(value = "b") int b,
+                                   @RequestParam(value = "operation") String operation,
+                                   Model model) {
+
+        model.addAttribute("result", calculator.getCalculation(a, b, operation));
+        return "simple/calculator";
     }
 }
